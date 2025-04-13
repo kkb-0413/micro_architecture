6ba9371da1df361a393a872a45cbc23e332473ee
kkb-0413 <kim010413@gmail.com>
2025-04-13 12:39:01 +0900

    feat : 전부 삭제

--- test2.sv
+++
@@ -1,7 +0,0 @@
-module (
-    input a,
-    input b,
-    output c
-);
-    assign c = a - b;
-endmodule
--- test3.sv
+++
@@ -1,9 +0,0 @@
-module (
-    input a,
-    input b,
-    output c,
-    output d
-);
-    assign c = a & b;
-    assign d = a | b;
-endmodule
